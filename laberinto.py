948652ea095bf9149e2067a11c03c3d845c26aee
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-11-28 12:16:15 +0100

    cambio

--- laberinto.py
+++ laberinto.py
@@ -6,6 +6,10 @@ laberinto = [
     ['X', 'X', 'X', 'X', 'S']
     ]
 
+hay_paso=('')
+hay_muro=('X')
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 
 no_muro = ((0,0), (1,0), (1,2), (1,3), (1,4), (2,0), (2,2), (2,4), (3,0), (3,1), (3,2), (3,4))
+
+['Abajo', 'Abajo', 'Abajo', 'Abajo', 'Derecha', 'Derecha', 'Arriba', 'Arriba', 'Derecha', 'Derecha',]
