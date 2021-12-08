7a0c27857507c733a9c2f323a1db9864229e6a24
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-08 17:39:06 +0100

    final

--- laberinto.py
+++ laberinto.py
@@ -10,10 +10,9 @@ laberinto_resuelto = [
 def creaLaberinto(muro, dimLab):
     
     laberinto = []
-    # Bucle para añadir las filas
     for i in range(dimLab):
         fila = []
-        #Bucle para añadir las columnas
+        
         for j in range(dimLab):
             if ((i==j) and (i==dimLab-1)):
                 fila.append('S')
@@ -23,7 +22,41 @@ def creaLaberinto(muro, dimLab):
                 fila.append(' ')
         laberinto.append(fila)
     return laberinto
+
+
+def recorreLaberinto(laberinto, numFilCol):
+   
+    movimientos = []
+    x=y=0
+    salir = False
+
+    while not salir:
+        if (x+1 < numFilCol) and (laberinto[x+1][y]!='X') and (len(movimientos) == 0 or movimientos[-1]!='Arriba'):
+            movimientos.append('Abajo')
+            x=x+1 
+            print("Avanza Abajo posicion", x,y)
+        elif (y+1 < numFilCol) and (laberinto[x][y+1]!='X') and (len(movimientos) == 0 or movimientos[-1]!='Izquierda'):
+
+            movimientos.append('Derecha')
+            y=y+1 
+            print("Avanza Derecha posicion ",x,y)
+        elif (y-1>0) and (y-1 < numFilCol) and (laberinto[x][y-1]!='X')and (len(movimientos) == 0 or movimientos[-1]!= 'Derecha'):
+            print("Avanza Izquierda")
+            movimientos.append('Izquierda')
+            y=y-1       
+            print("Avanza Izquierda posicion ",x,y)
+        elif (x-1>0) and (x-1 < numFilCol) and (laberinto[x-1][y]!='X') and (len(movimientos) == 0 or  movimientos[-1]!= 'Abajo'):
+            movimientos.append('Arriba')
+            x=x-1 
+            print("Avanza Arriba posicion", x,y)
+        elif (x==numFilCol-1) and (y==numFilCol-1) and (laberinto[x][y]=='S'):
+            salir = True
+    return movimientos
+
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 maxFilasyColumnas=5
-laberinto_resuelto=creaLaberinto(maxFilasyColumnas, muro)
-pprint.pprint(laberinto_resuelto)
+laberinto=creaLaberinto(muro, maxFilasyColumnas)
+pprint.pprint(laberinto)
+
+movimientos = recorreLaberinto(laberinto, maxFilasyColumnas)
+print("Recorrido del laberinto", movimientos)
