9ecdc0c0fc474c300630023487276d41c52c64bb
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-08 12:51:29 +0100

    cambio completo

--- laberinto.py
+++ laberinto.py
@@ -7,63 +7,7 @@ laberinto_resuelto = [
     [' ', ' ', ' ', 'X', ' '], 
     ['X', 'X', 'X', 'X', 'S']
     ]
-
-# Tarea 1
-def creaLaberinto(muro, dimLab):
-
-    laberinto = []
-    
-    for i in range(dimLab):
-        fila = []
-
-        for j in range(dimLab):
-            if ((i==j) and (i==dimLab-1)):
-                fila.append('S')
-            elif (tuple([i,j]) in muro):
-                fila.append('X')
-            else:    
-                fila.append(' ')
-        laberinto.append(fila)
-    return laberinto
-
-#Tarea 2
-def recorreLaberinto(laberinto, numFilCol):
-    
-    movimientos = []
-
-    x=y=0
-    salir = False
-
-
-    while not salir:
-        
-        if (x+1 < numFilCol) and (laberinto[x+1][y]!='X') and (len(movimientos) == 0 or movimientos[-1]!='Arriba'):
-            movimientos.append('Abajo')
-            x=x+1 
-            print("Avanza Abajo posicion", x,y)
-        elif (y+1 < numFilCol) and (laberinto[x][y+1]!='X') and (len(movimientos) == 0 or movimientos[-1]!='Izquierda'):
-            
-            movimientos.append('Derecha')
-            y=y+1 
-            print("Avanza Derecha posicion ",x,y)
-        elif (y-1>0) and (y-1 < numFilCol) and (laberinto[x][y-1]!='X')and (len(movimientos) == 0 or movimientos[-1]!= 'Derecha'):
-            
-            print("Avanza Izquierda")
-            movimientos.append('Izquierda')
-            y=y-1    
-            print("Avanza Izquierda posicion ",x,y)
-        elif (x-1>0) and (x-1 < numFilCol) and (laberinto[x-1][y]!='X') and (len(movimientos) == 0 or  movimientos[-1]!= 'Abajo'):
-            
-            movimientos.append('Arriba')
-            x=x-1 
-            print("Avanza Arriba posicion", x,y)
-        elif (x==numFilCol-1) and (y==numFilCol-1) and (laberinto[x][y]=='S'):
-            
-            salir = True
-    return movimientos
-
-# Tarea 1. Crear laberinto 
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-maxFilCol = 5
-laberinto = creaLaberinto(muro, maxFilCol)
-pprint.pprint(laberinto)
+maxFilasyColumnas=5
+laberinto_resuelto=creaLaberinto(maxFilasyColumnas, muro)
+pprint.pprint(laberinto_resuelto)
